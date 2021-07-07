9561d423888270243f0342bdb4091b15cd78cf90
filip123go <stavros489@hotmail.com>
2021-07-07 23:23:01 +0300

    models added

--- app/src/main/java/com/stavros/weather_aplication/MainActivity.java
+++ app/src/main/java/com/stavros/weather_aplication/MainActivity.java
@@ -2,17 +2,42 @@ package com.stavros.weather_aplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.google.android.material.floatingactionbutton.ExtendedFloatingActionButton;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class MainActivity extends AppCompatActivity {
 
     private TextView city, temperature, weatherCondition, humidity, maxTemperature, minTemperature, pressure, wind;
+    private ImageView imageView;
+    private FloatingActionButton fab;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        city = findViewById(R.id.textViewCity);
+        temperature = findViewById(R.id.textViewTemp);
+        weatherCondition = findViewById(R.id.textViewWeatherCondition);
+        humidity = findViewById(R.id.textViewHumidity);
+        maxTemperature = findViewById(R.id.textViewMaxTemp);
+        minTemperature = findViewById(R.id.textViewMinTemp);
+        pressure = findViewById(R.id.textViewPressure);
+        wind = findViewById(R.id.textViewWind);
+        imageView = findViewById(R.id.imageView);
+        fab = findViewById(R.id.fab);
+
+        fab.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, WeatherActivity.class);
+            startActivity(intent);
+        });
+
     }
 }
--- app/src/main/java/com/stavros/weather_aplication/WeatherActivity.java
+++ app/src/main/java/com/stavros/weather_aplication/WeatherActivity.java
@@ -3,12 +3,41 @@ package com.stavros.weather_aplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class WeatherActivity extends AppCompatActivity {
+
+
+    private TextView cityWeather, temperatureWeather, condition, humidityWeather, maxTemperatureWeather, minTemperatureWeather, pressureWeather, windWeather;
+    private ImageView imageViewWeather;
+    private Button search;
+    private EditText editText;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_weather);
+
+
+        cityWeather = findViewById(R.id.textViewCityWeather);
+        temperatureWeather = findViewById(R.id.textViewTempWeather);
+        condition = findViewById(R.id.textViewWeatherConditionWeather);
+        humidityWeather = findViewById(R.id.textViewHumidityWeather);
+        maxTemperatureWeather = findViewById(R.id.textViewMaxTempWeather);
+        minTemperatureWeather = findViewById(R.id.textViewMinTempWeather);
+        pressureWeather = findViewById(R.id.textViewPressureWeather);
+        windWeather = findViewById(R.id.textViewWindWather);
+        imageViewWeather = findViewById(R.id.imageViewWeather);
+        search = findViewById(R.id.search);
+        editText = findViewById(R.id.editTextCityName);
+
+        search.setOnClickListener(v -> {
+            String cityName = editText.getText().toString();
+        });
+
     }
 }
